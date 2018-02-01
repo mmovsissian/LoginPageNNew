624834845b3679a0f78be72e42783df9eee5ba69
Movses <mmovsissian@gmail.com>
2018-02-01 17:44:10 +0300

    part

---
+++ app/src/main/java/com/example/pink24/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.pink24.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText password;
+    private Button ok;
+    private Button register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login=findViewById(R.id.login);
+        password=findViewById(R.id.password);
+        ok=findViewById(R.id.ok);
+        register=findViewById(R.id.register);
+
+
+        register.setOnClickListener(new  View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
+
+//                SharedPreferences sharedPreferences = getSharedPreferences("Login", MODE_PRIVATE);
+//                SharedPreferences.Editor editor= sharedPreferences.edit();
+//
+//                        )
+
+            }
+                              });
+
+//                startActivity(intent);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/pink24/myapplication/RegistrationActivity.java
@@ -0,0 +1,40 @@
+package com.example.pink24.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+ private   Button reg_ok;
+  private   EditText reg_login;
+  private  EditText reg_pass;
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+            reg_ok.findViewById(R.id.password);
+
+
+
+
+
+
+
+
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences("Login", MODE_PRIVATE);
+//        SharedPreferences.Editor editor= sharedPreferences.edit();
+//
+
+    }
+}
